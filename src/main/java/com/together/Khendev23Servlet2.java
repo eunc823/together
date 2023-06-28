b186c716f40e6d375bb52a122c92f9429736d0f5
khendev23 <khendev23@gmail.com>
2023-06-28 12:46:06 +0900

    khendev23Servlet2 추가!

---
+++ src/main/java/com/together/Khendev23Servlet2.java
@@ -0,0 +1,25 @@
+package com.together;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Khendev23Servlet2
+ */
+@WebServlet("/khendev23ver2")
+public class Khendev23Servlet2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.getWriter().append("오늘 점심은 육전식당 냉면이다.");
+	}
+
+}
