6ba55073384ddcdca1ce4b4613a4c978f64a1d83
hongseungyoung <tmddud1532@naver.com>
2023-06-28 13:39:46 +0900

    hongseungyoung url zz

--- src/main/java/com/together/HongseungyoungServlet2.java
+++ src/main/java/com/together/HongseungyoungServlet2.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class HongseungyoungServlet2
  */
-@WebServlet("/hongseungyoung")
+@WebServlet("/hongseungyoung222")
 public class HongseungyoungServlet2 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
